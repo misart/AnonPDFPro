7b89f5d98187aec60c0edce9c71dfcf4be84f5d5
Sławomir Klimek <sklimek@um.szczecin.pl>
2026-01-22 12:24:49 +0100

    chore(version): bump to 1.26.0122.5 - update app version metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.26.0114.4")]
+[assembly: AssemblyFileVersion("1.26.0122.5")]
 [assembly: NeutralResourcesLanguage("pl-PL")]
